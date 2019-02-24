50e468e998813f83e6697ebed9c0979d857b33a9
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2019-02-24 01:15:25 -0600

    Added XUnitTest Project to the PlanYourDegree solution

---
+++ WS02_PlanYourDegree/PlanYourDegree/Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanYourDegree.Models
+{
+    public class TestModel
+    {
+        public int AddTwoInt(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
