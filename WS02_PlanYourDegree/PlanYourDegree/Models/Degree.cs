4b3f193b5b499a29a3042e4c47b64db913d02d62
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2019-02-27 09:11:16 -0600

    created the domain model fro degree

---
+++ WS02_PlanYourDegree/PlanYourDegree/Models/Degree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanYourDegree.Models
+{
+    public class Degree
+    {
+        public int DegreeID { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string DegreeAbrev { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string DegreeName { get; set; }
+    }
+}
